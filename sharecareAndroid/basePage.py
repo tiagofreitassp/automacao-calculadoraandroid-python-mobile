3ba255510dd2a31a9e62466730a80baa90a9f499
Tiago Freitas <tiago.freitas.sp@gmail.com>
2020-02-29 11:04:09 -0300

    Remocao de script nulo

--- sharecareAndroid/basePage.py
+++
@@ -1,8 +0,0 @@
-from time import sleep
-from appium.webdriver.common.touch_action import TouchAction
-
-def scrollDown(self):
-        sleep(1)
-        touch = TouchAction(self.driver)
-        touch.press(x=290, y=840).move_to(x=292, y=191).release().perform()
-        sleep(1)
